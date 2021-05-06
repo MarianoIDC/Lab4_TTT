c440cc75da868b77825a9f55a28723f3f55415dd
MarianoIDC <mmunoz@estudiantec.cr>
2021-05-05 21:49:51 -0600

    Cvaz pruebe esto

--- vga_controller.sv
+++ vga_controller.sv
@@ -1,8 +1,8 @@
 module vga_controller(
 	
-	input logic clk,
+	input logic clk,rst,
 	output logic Hsynq,
-	output logic Vsynq,
+	output logic Vsynq,blank, sync,
 	output logic [7:0] Red,
 	output logic [7:0] Green,
 	output logic [7:0] Blue, 
@@ -12,6 +12,6 @@ module vga_controller(
 logic clk_25M;	
 clock_divider VGA_Clock_gen (clk, clk_25M);
 
-vga_controller_final vcg (clk_25M, Hsynq, Vsynq, clk_vga, Red, Green, Blue); 
+vga_controller_final vcg (clk_25M, rst, Hsynq, Vsynq, clk_vga, blank, sync, Red, Green, Blue); 
 
 endmodule
--- vga_controller_final.sv
+++ vga_controller_final.sv
@@ -1,17 +1,32 @@
 module vga_controller_final(
-	input  logic CLK_IN,           // 25 MHz
+	input logic CLK_IN,	// 25 MHz
+	input logic RST, 
 	output logic o_hsync,      // horizontal sync
    output logic o_vsync,       // vertical sync
    output logic CLK_OUT,
-   output logic [7:0] red,
-   output logic [7:0] blue,
-   output logic [7:0] green );
+	output logic BLANK, 
+	output logic SYNC,
+   output logic [7:0] R,
+   output logic [7:0] G,
+   output logic [7:0] B );
 
 	
+	parameter int HD = 640;
+	parameter int HFP = 16;
+	parameter int HSP = 96;
+	parameter int HBP = 48;
+	
+	parameter int VD = 480;
+	parameter int VFP = 10;
+	parameter int VSP = 2;
+	parameter int VBP = 33;
+	
+	
+	logic de = 0;
+	
+		
 	logic [9:0]  counterX = 0;
 	logic [9:0]  counterY = 0;
-	
-	
 	
 	
 	always@(posedge CLK_IN) begin
@@ -38,10 +53,158 @@ module vga_controller_final(
 	
 	assign CLK_OUT = CLK_IN;
 	
-	assign Red = (counterX < 783 && counterX > 144 && counterY < 514 && counterY > 35) ? 8'hF:8'h0; 
-	assign Green = (counterX < 783 && counterX > 144 && counterY < 514 && counterY > 35) ? 8'hF:8'h0; 
-	assign Blue = (counterX < 783 && counterX > 144 && counterY < 514 && counterY > 35) ? 8'hF:8'h0; 
-
+	//assign Red = (counterX < 784 && counterX > 143 && counterY < 515 && counterY > 34) ? 8'b11111111:8'b00000000; 
+	//assign Green = (counterX < 784 && counterX > 143 && counterY < 515 && counterY > 34) ? 8'b11111111:8'b00000000; 
+	//assign Blue = (counterX < 784 && counterX > 143 && counterY < 515 && counterY > 34) ? 8'b11111111:8'b00000000; 
+	// video_on
+  always_ff @(posedge CLK_IN, negedge RST)
+    if (~RST)
+      begin
+        de    <= 0;
+        BLANK <= 0;
+      end
+    else if (CLK_IN)
+      begin
+        if(counterX < HD && counterY < VD)
+          de <= 1;
+        else
+          de <= 0;
+        BLANK <= de; // delay one clock to align with pixel data
+      end
+    
+  // draw
+  always_ff @(posedge CLK_IN, negedge RST)
+    if (~RST)
+      begin
+        R <= 8'b00000000;
+        G <= 8'b00000000;
+        B <= 8'b00000000;
+      end
+    else if (CLK_IN)  
+      begin 
+        
+        // Si [0][0] y 0
+        // Entonces dibujar en el primer cuadro una
+		  // Lineas l en la primer fila
+        if (counterX>105 && counterX<110 && counterY>30 && counterY<130)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        else if(counterX>320 && counterX<325 && counterY>30 && counterY<130)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        else if(counterX>535 && counterX<540 && counterY>30 && counterY<130)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        
+        // Lineas l en la segunda fila
+        else if(counterX>105 && counterX<110 && counterY>192 && counterY<292)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        else if(counterX>320 && counterX<325 && counterY>192 && counterY<292)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        else if(counterX>535 && counterX<540 && counterY>192 && counterY<292)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+         
+        // Lineas l en la tercer fila
+        else if(counterX>105 && counterX<110 && counterY>349 && counterY<449)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        else if(counterX>320 && counterX<325 && counterY>349 && counterY<449)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        else if(counterX>535 && counterX<540 && counterY>349 && counterY<449)
+            begin
+              R <= 8'b11111111;
+              G <= 8'b00000000;
+              B <= 8'b00000000;
+            end
+        
+        // Linea - en la primer fila    
+        else if (counterX>30 && counterX<180 && counterY>78 && counterY<83)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        else if (counterX>245 && counterX<395 && counterY>78 && counterY<83)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        else if (counterX>460 && counterX<610 && counterY>78 && counterY<83)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        
+        // Linea - en la segunda fila    
+        else if (counterX>30 && counterX<180 && counterY>240 && counterY<245)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        else if (counterX>245 && counterX<395 && counterY>240 && counterY<245)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        else if (counterX>460 && counterX<610 && counterY>240 && counterY<245)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        
+        // Linea - en la tercer fila    
+        else if (counterX>30 && counterX<180 && counterY>402 && counterY<407)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        else if (counterX>245 && counterX<395 && counterY>402 && counterY<407)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+        else if (counterX>460 && counterX<610 && counterY>402 && counterY<407)
+              begin
+                R <= 8'b00000000;
+                G <= 8'b00000000;
+                B <= 8'b11111111;
+              end
+		end
 		
 
-endmodule
+endmodule 		
